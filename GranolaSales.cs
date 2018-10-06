a83776fb721f538b6511fa55abd369e0adf46b2a
mclark85 <33467276+mclark85@users.noreply.github.com>
2018-10-05 20:53:57 -0500

    Add files via upload

---
+++ GranolaSales.cs
@@ -0,0 +1,51 @@
+﻿/*Matt Clark
+ * Program 2 Due: January 23, 2018
+ * None
+ * This will program will calculate information about a Gronala Bar sale including total made, total profits, percentage to give away and amounts sold.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MClark_Prog2
+{
+    class GranolaSales
+    {
+        static void Main(string[] args)
+        {
+            const int CASES_SOLD = 32;
+            const double GOV_PERCENT = 0.1275;
+            decimal costPerCase = 110m;
+            decimal costPerBar = 1.50m;
+            int numBarsPerCase = 100;
+            decimal totCost = costPerCase * CASES_SOLD;
+            int totBars = CASES_SOLD * numBarsPerCase;
+            decimal grossIncome = costPerBar * totBars;
+            decimal netIncome = grossIncome - totCost;
+            decimal govCut = netIncome * (decimal) GOV_PERCENT;
+            decimal finalProfit = netIncome - govCut;
+
+
+            WriteLine("Welcome to the IWCC Nerd Squad Granola Sales Project Final Report");
+            WriteLine();
+            WriteLine("Number of cases of granola sold: {0, 37:F0}", CASES_SOLD );
+            WriteLine("Cost per case: {0, 55:C}", costPerCase );
+            WriteLine("Total cost incurred: {0, 49:C}", totCost );
+            WriteLine("Number of bars in each case: {0, 41:F0}", numBarsPerCase );
+            WriteLine("Total number of bars were sold: {0, 38:F0}", totBars );
+            WriteLine("Cost per bar: {0, 56:C}", costPerBar );
+            WriteLine("Gross income:{0, 57:C}", grossIncome );
+            WriteLine("Net income <Gross income - Total cost incurred>:{0, 22:C}", netIncome );
+            WriteLine("Percent to be given to student government: {0, 27:P}", GOV_PERCENT );
+            WriteLine("Amount of net income to be given to student government: {0, 14:C}", govCut );
+            WriteLine();
+            WriteLine("Final Profit for the Nerd Squad: {0, 37:C}", finalProfit );
+
+        }
+    }
+}
---
+++ TipSplit.cs
@@ -0,0 +1,93 @@
+﻿/*Matt Clark
+ * Program 4 Due: February 6, 2018
+ * None
+ * This program will accept user input for the number of people and the bill of eating out and then calculate the total with different tip amounts and how much it is if split evenly.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MClark_Prog4
+{
+    class TipSplit
+    {
+        static void Main(string[] args)
+        {
+            const double TIP_15 = 0.15;
+            const double TIP_20 = 0.20;
+            decimal tip15Calc;
+            decimal tip20Calc;
+            decimal tip15Tot;
+            decimal tip20Tot;
+            decimal tip15Share;
+            decimal tip20Share;
+
+            Greeting();
+            decimal billTot = GetTotal();
+            int numGuests = GetPeople();
+            CalcTip(TIP_15, TIP_20, billTot, out tip15Calc, out tip20Calc);
+            CalcTotal(billTot, tip15Calc, tip20Calc, out tip15Tot, out tip20Tot);
+            SplitBill(numGuests, tip15Tot, tip20Tot, out tip15Share, out tip20Share);
+            PrintReceipt(tip15Calc, tip20Calc, tip15Tot, tip20Tot, tip15Share, tip20Share);
+        }
+
+
+        public static void Greeting()
+        {
+            WriteLine("Welcome to Tiny's Pub Tip Calculator.");
+            WriteLine();
+        }
+
+        public static decimal GetTotal()
+        {
+            Write("Enter the bill total:  ");
+            decimal bill = decimal.Parse(ReadLine());
+            return bill;
+        }
+
+        public static void CalcTip(double lowTip, double highTip, decimal bill, out decimal lowTipCalc, out decimal highTipCalc)
+        {
+            lowTipCalc = bill * (decimal)lowTip;
+            highTipCalc = bill * (decimal)highTip;
+
+        }
+
+        public static void CalcTotal(decimal bill, decimal lowTipCalc, decimal highTipCalc, out decimal lowTipTot, out decimal highTipTot)
+        {
+            lowTipTot = bill + lowTipCalc;
+            highTipTot = bill + highTipCalc;
+        }
+
+        public static int GetPeople()
+        {
+            Write("How many people in your party:  ");
+            int guests = int.Parse(ReadLine());
+            WriteLine();
+            return guests;
+        }
+
+        public static void SplitBill(int guests, decimal lowTipTot, decimal highTipTot, out decimal lowShare, out decimal highShare)
+        {
+            lowShare = lowTipTot / guests;
+            highShare = highTipTot / guests;
+        }
+
+        public static void PrintReceipt(decimal lowTipCalc, decimal highTipCalc, decimal lowTipTot, decimal highTipTot, decimal lowShare, decimal highShare)
+        {
+            WriteLine("15% tip would be: {0, 21:C}", lowTipCalc);
+            WriteLine("20% tip would be: {0, 21:C}", highTipCalc);
+            WriteLine("");
+
+            WriteLine("Total for 15% tip would be: {0, 11:C}", lowTipTot);
+            WriteLine("Each person's share is: {0, 15:C}", lowShare);
+            WriteLine("");
+
+            WriteLine("Total for 20% tip would be: {0, 11:C}", highTipTot);
+            WriteLine("Each person's share is: {0, 15:C}", highShare);
+        }
+    }
+}
