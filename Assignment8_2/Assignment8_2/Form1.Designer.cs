3d9e24f6564f7fbb5a81d35cc44ab9293d37aa60
mclark85 <33467276+mclark85@users.noreply.github.com>
2020-07-26 20:38:35 -0500

    Add files via upload

---
+++ Assignment8_2/Assignment8_2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Assignment8_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.instruction = new System.Windows.Forms.Label();
+            this.hint = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(68, 72);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(111, 72);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(154, 72);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(37, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(197, 72);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(37, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(240, 72);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(37, 21);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.RadioButton5_CheckedChanged);
+            // 
+            // instruction
+            // 
+            this.instruction.AutoSize = true;
+            this.instruction.Location = new System.Drawing.Point(108, 18);
+            this.instruction.Name = "instruction";
+            this.instruction.Size = new System.Drawing.Size(120, 17);
+            this.instruction.TabIndex = 5;
+            this.instruction.Text = "Choose a number";
+            // 
+            // hint
+            // 
+            this.hint.AutoSize = true;
+            this.hint.Cursor = System.Windows.Forms.Cursors.Default;
+            this.hint.Location = new System.Drawing.Point(151, 130);
+            this.hint.Name = "hint";
+            this.hint.Size = new System.Drawing.Size(33, 17);
+            this.hint.TabIndex = 6;
+            this.hint.Text = "Hint";
+            this.hint.MouseHover += new System.EventHandler(this.Label1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 198);
+            this.Controls.Add(this.hint);
+            this.Controls.Add(this.instruction);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label instruction;
+        private System.Windows.Forms.Label hint;
+    }
+}
+
---
+++ Assignment8_2/Assignment8_2/Form1.cs
@@ -0,0 +1,132 @@
+﻿/* Matt Clark
+ * 7/26/2020
+ * Assignment 8.2
+ * This program allows a user to guess a number from program while providing hints here and there.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment8_2
+{
+    public partial class Form1 : Form
+    {
+        int winner; 
+        int guessRadio; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            guessRadio = 1;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton5.Enabled = false;
+            Check();
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            guessRadio = 2;
+            radioButton1.Enabled = false;
+            radioButton3.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton5.Enabled = false;
+            Check();
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            guessRadio = 3;
+            radioButton2.Enabled = false;
+            radioButton1.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton5.Enabled = false;
+            Check();
+        }
+
+        private void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            guessRadio = 4;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            radioButton1.Enabled = false;
+            radioButton5.Enabled = false;
+            Check();
+        }
+
+        private void RadioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            guessRadio = 5;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton1.Enabled = false;
+            Check();
+        }
+
+        private void Label1_MouseHover(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int hintNumber = rand.Next(1, 6);
+
+            while (hintNumber == winner)
+            {
+                hintNumber = rand.Next(1, 6);
+            }
+            MessageBox.Show(hintNumber + "- This button is incorrect");
+
+            if (hintNumber == 1)
+            {
+                radioButton1.Enabled = false;
+            }
+            if (hintNumber == 2)
+            {
+                radioButton2.Enabled = false;
+            }
+            if (hintNumber == 3)
+            {
+                radioButton3.Enabled = false;
+            }
+            if (hintNumber == 4)
+            {
+                radioButton4.Enabled = false;
+            }
+            if (hintNumber == 5)
+            { 
+                radioButton5.Enabled = false;
+            }
+            hint.Enabled = false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            winner = rand.Next(1, 7);
+        }
+
+        private void Check()
+        {
+            if (winner == guessRadio)
+            {
+                MessageBox.Show("Correct guess");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect guess");
+            }
+        }
+    }
+}
